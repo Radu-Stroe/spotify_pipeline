3a893d9eba12cb85cb74bcc3112d1c0bd12619ed
“Radu-Stroe” <stroeradu03@gmail.com>
2025-03-19 20:26:41 +0200

    Revert dbt changes

--- dbt/models/dim/dim_countries.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-WITH countries AS (
-    SELECT DISTINCT
-        COALESCE(country, 'Global') AS country_id
-    FROM {{ ref('stg_spotify') }}
-)
-
-SELECT * FROM countries
